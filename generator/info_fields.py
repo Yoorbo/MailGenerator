a79a7fcbc49442aa9ba163cbca7a9af3a8f6f3d1
Yorb <yannberton.yb@gmail.com>
2021-12-16 12:25:37 +0100

    More inline documentation
    Took 5 minutes

--- generator/info_fields.py
+++ generator/info_fields.py
@@ -1,14 +1,29 @@
 class Sender:
+    """
+        Definition for data construct that stores relevant sender information
+    """
     def __init__(self):
         pass
 
 
 class Date:
+    """
+        Definition for data construct that stores the date
+    """
     def __init__(self):
+        pass
+
+    def fill_requirements(self):
+        pass
+
+    def get_string(self):
         pass
 
 
 class Adress:
+    """
+        Definition for data construct that stores relevant adress information. e.g. mail, cc bcc
+    """
     def __init__(self):
         pass
 
